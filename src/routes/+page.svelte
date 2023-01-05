45c9abcce5e4bf49034934aa0f4a000701326100
AMAGHZAZ-Y <yassine.amaghzaz@outlook.com>
2023-01-05 21:49:51 +0100

    Initial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+	import DCalc from "$lib/components/DCalc.svelte";
+	import MotorsSetStep from "$lib/components/MotorsSetStep.svelte";
+	import SpeedAccel from "$lib/components/SpeedAccel.svelte";
+	import { Column, Grid, Row } from "carbon-components-svelte";
+</script>
+
+<Grid fullWidth>
+	<Row noGutter>
+		<Column>
+			<MotorsSetStep />
+		</Column>
+	</Row>
+	<Row noGutter>
+		<Column>
+			<SpeedAccel />
+		</Column>
+	</Row>
+	<Row noGutter>
+		<Column>
+			<DCalc />
+		</Column>
+	</Row>
+</Grid>
